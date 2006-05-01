79e73c2087f4ba8a9dbdf628f68be8609963705e
xhuang <sha1-4bd1aee960316b9472cdc249dc0e7db0c29cbfd7@cern.ch>
2006-05-01 23:29:17 +0000

    add more histograms

--- test/SiStripDigiPlots.C
+++ test/SiStripDigiPlots.C
@@ -23,14 +23,14 @@ Char_t histo[200];
    TH1* meAdcTIB_[4];
 
    for (Int_t i=0; i<4; i++){
-      sprintf(histo,"DQMData/adc_tib_%d;1",i+1);
+      sprintf(histo,"DQMData/adc_tib_%d_zm;1",i+1);
       rfile->GetObject(histo ,meAdcTIB_[i]);
       meAdcTIB_[i];
       Strip->cd(i+1);
       meAdcTIB_[i]->Draw();
    }
 
-   Strip->Print("AdcOfStripTIB.eps"); 
+   Strip->Print("AdcOfStripTIB_ZM.eps"); 
  }
 
  if (1) {
@@ -40,16 +40,68 @@ Char_t histo[200];
    TH1* meStripTIB_[3];
 
    for (Int_t i=0; i<3; i++){
-      sprintf(histo,"DQMData/strip_tib_%d;1",i+1);
+      sprintf(histo,"DQMData/strip_tib_%d_zm;1",i+1);
       rfile->GetObject(histo ,meStripTIB_[i]);
       meStripTIB_[i];
       Strip->cd(i+1);
       meStripTIB_[i]->Draw();
    }
 
-   Strip->Print("StripNumOfStripTIB.eps");
+   Strip->Print("StripNumOfStripTIB_ZM.eps");
  }
 
+ if (1) {
+  TCanvas * Strip = new TCanvas("Strip","Strip",1000,1000);
+   Strip->Divide(2,2);
+
+   TH1* meAdcTIB_[4];
+
+   for (Int_t i=0; i<4; i++){
+      sprintf(histo,"DQMData/adc_tib_%d_zp;1",i+1);
+      rfile->GetObject(histo ,meAdcTIB_[i]);
+      meAdcTIB_[i];
+      Strip->cd(i+1);
+      meAdcTIB_[i]->Draw();
+   }
+
+   Strip->Print("AdcOfStripTIB_ZP.eps");
+ }
+
+ if (1) {
+  TCanvas * Strip = new TCanvas("Strip","Strip",1000,1000);
+   Strip->Divide(2,2);
+
+   TH1* meStripTIB_[4];
+
+   for (Int_t i=0; i<4; i++){
+      sprintf(histo,"DQMData/strip_tib_%d_zp;1",i+1);
+      rfile->GetObject(histo ,meStripTIB_[i]);
+      meStripTIB_[i];
+      Strip->cd(i+1);
+      meStripTIB_[i]->Draw();
+   }
+
+   Strip->Print("StripNumOfStripTIB_ZP.eps");
+ }
+
+/*
+ if (1) {
+  TCanvas * Strip = new TCanvas("Strip","Strip",1000,1000);
+   Strip->Divide(2,2);
+
+   TH1* meNdigiTIB_[4];
+
+   for (Int_t i=0; i<4; i++){
+      sprintf(histo,"DQMData/ndigi_tib_layer_%d_zm;1",i+1);
+      rfile->GetObject(histo ,meNdigiTIB_[i]);
+      meNdigiTIB_[i];
+      Strip->cd(i+1);
+      meNdigiTIB_[i]->Draw();
+   }
+
+   Strip->Print("NdigiTIB_ZP.eps");
+ }
+*/
 // TOB
  if(1) {
    TCanvas * Strip = new TCanvas("Strip","Strip",800,1000);
@@ -58,13 +110,13 @@ Char_t histo[200];
    TH1* meAdcTOB_[6];
 
    for (Int_t i=0; i<6; i++){
-      sprintf(histo,"DQMData/adc_tob_%d;1",i+1);
+      sprintf(histo,"DQMData/adc_tob_%d_zm;1",i+1);
       rfile->GetObject(histo ,meAdcTOB_[i]);
       meAdcTOB_[i];
       Strip->cd(i+1);
       meAdcTOB_[i]->Draw();
    }
-   Strip->Print("AdcOfStripTOB.eps");
+   Strip->Print("AdcOfStripTOB_ZM.eps");
  }
 
  if (1) {
@@ -73,17 +125,50 @@ Char_t histo[200];
    TH1* meStripTOB_[6];
 
    for (Int_t i=0; i<6; i++){
-      sprintf(histo,"DQMData/strip_tob_%d;1",i+1);
+      sprintf(histo,"DQMData/strip_tob_%d_zm;1",i+1);
       rfile->GetObject(histo ,meStripTOB_[i]);
       meStripTOB_[i];
       Strip->cd(i+1);
       meStripTOB_[i]->Draw();
    }
 
-   Strip->Print("StripNumOfStripTOB.eps");
+   Strip->Print("StripNumOfStripTOB_ZM.eps");
  }
 
  
+if(1) {
+   TCanvas * Strip = new TCanvas("Strip","Strip",800,1000);
+   Strip->Divide(2,3);
+
+   TH1* meAdcTOB_[6];
+
+   for (Int_t i=0; i<6; i++){
+      sprintf(histo,"DQMData/adc_tob_%d_zp;1",i+1);
+      rfile->GetObject(histo ,meAdcTOB_[i]);
+      meAdcTOB_[i];
+      Strip->cd(i+1);
+      meAdcTOB_[i]->Draw();
+   }
+   Strip->Print("AdcOfStripTOB_ZP.eps");
+ }
+
+ if (1) {
+  TCanvas * Strip = new TCanvas("Strip","Strip",800,1000);
+   Strip->Divide(2,3);
+   TH1* meStripTOB_[6];
+
+   for (Int_t i=0; i<6; i++){
+      sprintf(histo,"DQMData/strip_tob_%d_zp;1",i+1);
+      rfile->GetObject(histo ,meStripTOB_[i]);
+      meStripTOB_[i];
+      Strip->cd(i+1);
+      meStripTOB_[i]->Draw();
+   }
+
+   Strip->Print("StripNumOfStripTOB_ZP.eps");
+ }
+
+
 //TID
    if (1) {
    TCanvas * Strip = new TCanvas("Strip","Strip",800,1000);
@@ -91,14 +176,14 @@ Char_t histo[200];
    TH1* meAdcTID_[3];
 
    for (Int_t i=0; i<3; i++){
-      sprintf(histo,"DQMData/adc_tid_%d;1",i+1);
+      sprintf(histo,"DQMData/adc_tid_%d_zm;1",i+1);
       rfile->GetObject(histo ,meAdcTID_[i]);
       meAdcTID_[i];
       Strip->cd(i+1);
       meAdcTID_[i]->Draw();
    }
 
-   Strip->Print("AdcOfStripTID.eps");
+   Strip->Print("AdcOfStripTID_ZM.eps");
    }
 
    if (1) {
@@ -107,14 +192,46 @@ Char_t histo[200];
    TH1* meStripTID_[3];
 
    for (Int_t i=0; i<3; i++){
-      sprintf(histo,"DQMData/strip_tid_%d;1",i+1);
+      sprintf(histo,"DQMData/strip_tid_%d_zm;1",i+1);
       rfile->GetObject(histo ,meStripTID_[i]);
       meStripTID_[i];
       Strip->cd(i+1);
       meStripTID_[i]->Draw();
    }
 
-   Strip->Print("StripNumOfStripTID.eps");
+   Strip->Print("StripNumOfStripTID_ZM.eps");
+   }
+
+   if (1) {
+   TCanvas * Strip = new TCanvas("Strip","Strip",800,1000);
+   Strip->Divide(1,3);
+   TH1* meAdcTID_[3];
+
+   for (Int_t i=0; i<3; i++){
+      sprintf(histo,"DQMData/adc_tid_%d_zp;1",i+1);
+      rfile->GetObject(histo ,meAdcTID_[i]);
+      meAdcTID_[i];
+      Strip->cd(i+1);
+      meAdcTID_[i]->Draw();
+   }
+
+   Strip->Print("AdcOfStripTID_ZP.eps");
+   }
+
+   if (1) {
+   TCanvas * Strip = new TCanvas("Strip","Strip",800,1000);
+   Strip->Divide(1,3);
+   TH1* meStripTID_[3];
+
+   for (Int_t i=0; i<3; i++){
+      sprintf(histo,"DQMData/strip_tid_%d_zp;1",i+1);
+      rfile->GetObject(histo ,meStripTID_[i]);
+      meStripTID_[i];
+      Strip->cd(i+1);
+      meStripTID_[i]->Draw();
+   }
+
+   Strip->Print("StripNumOfStripTID_ZP.eps");
    }
 
 //IEC
@@ -124,14 +241,14 @@ Char_t histo[200];
    TH1* meAdcTEC_[9];
 
    for (Int_t i=0; i<9; i++){
-      sprintf(histo,"DQMData/adc_tec_%d;1",i+1);
+      sprintf(histo,"DQMData/adc_tec_%d_zm;1",i+1);
       rfile->GetObject(histo ,meAdcTEC_[i]);
       meAdcTEC_[i];
       Strip->cd(i+1);
       meAdcTEC_[i]->Draw();
    }
 
-   Strip->Print("AdcOfStripTEC.eps");
+   Strip->Print("AdcOfStripTEC_ZM.eps");
    } 
 
    if (1) {
@@ -140,14 +257,47 @@ Char_t histo[200];
    TH1* meStripTEC_[9];
 
    for (Int_t i=0; i<9; i++){
-      sprintf(histo,"DQMData/strip_tec_%d;1",i+1);
+      sprintf(histo,"DQMData/strip_tec_%d_zm;1",i+1);
       rfile->GetObject(histo ,meStripTEC_[i]);
       meStripTEC_[i];
       Strip->cd(i+1);
       meStripTEC_[i]->Draw();
    }
 
-   Strip->Print("StripNumOfStripTEC.eps");
+   Strip->Print("StripNumOfStripTEC_ZM.eps");
    }
+
+   if (1) {
+   TCanvas * Strip = new TCanvas("Strip","Strip",1000,1000);
+   Strip->Divide(3,3);
+   TH1* meAdcTEC_[9];
+
+   for (Int_t i=0; i<9; i++){
+      sprintf(histo,"DQMData/adc_tec_%d_zp;1",i+1);
+      rfile->GetObject(histo ,meAdcTEC_[i]);
+      meAdcTEC_[i];
+      Strip->cd(i+1);
+      meAdcTEC_[i]->Draw();
+   }
+
+   Strip->Print("AdcOfStripTEC_ZP.eps");
+   }
+
+   if (1) {
+   TCanvas * Strip = new TCanvas("Strip","Strip",1000,1000);
+   Strip->Divide(3,3);
+   TH1* meStripTEC_[9];
+
+   for (Int_t i=0; i<9; i++){
+      sprintf(histo,"DQMData/strip_tec_%d_zp;1",i+1);
+      rfile->GetObject(histo ,meStripTEC_[i]);
+      meStripTEC_[i];
+      Strip->cd(i+1);
+      meStripTEC_[i]->Draw();
+   }
+
+   Strip->Print("StripNumOfStripTEC_ZP.eps");
+   }
+
 }
 
